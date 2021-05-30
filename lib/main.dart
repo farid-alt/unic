c0bc1da38f6c75bf7cfea34fa2f1b89a47346f9e
Rustam2707 <rustamazizov2707@gmail.com>
2021-05-30 14:22:40 +0400

    May 30 05 (2)

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:unic_app/views/contact%20us/contact_us.dart';
-import 'package:unic_app/views/no%20internet/no_internet_page.dart';
-import 'package:unic_app/views/onboarding/onboarding_view.dart';
-import 'package:unic_app/views/user/code_page/code_page_view.dart';
-import 'package:unic_app/views/user/code_page/code_page_viewmodel.dart';
-import 'package:unic_app/views/user/enter%20promo/enter_promo_view.dart';
-import 'package:unic_app/views/user/get_rides/get_rides_view.dart';
-import 'package:unic_app/views/user/main_wrapper/main_wrapper_view.dart';
-import 'package:unic_app/views/user/privacy%20and%20policy/privacy.dart';
-import 'package:unic_app/views/user/profile_page/profile%20edit/profile_edit_view.dart';
-import 'package:unic_app/views/user/profile_page/profile_page_view.dart';
-import 'package:unic_app/views/user/promotions_page/promotions_view.dart';
-import 'package:unic_app/views/user/ride_history/ride_history_view.dart';
+
 import 'package:unic_app/views/user/support/support_view.dart';
-import 'package:unic_app/views/user/terms%20and%20conditions/terms_view.dart';
 
 void main() {
   initializeDateFormatting();
@@ -26,22 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(fontFamily: 'Inter'),
-        // home: OnboardingView(),
-        home: SupportView()
-        //home: UserProfileEditPageView(),
-        );
-=======
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Inter'),
       // home: OnboardingView(),
-      home: MainWrapperView(
-        index: 0,
-      ),
+      home: SupportView(),
       //home: UserProfileEditPageView(),
     );
->>>>>>> 537cc23f0bed58442fe4b60ca1db2831dc872d8e
   }
 }
